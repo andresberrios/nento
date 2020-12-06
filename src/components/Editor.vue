7dee400c8b843e76687d2db5ab8dff27a67ccd95
andresberrios <andres.berrios.j@gmail.com>
2020-12-06 18:22:06 +0100

    Clean up

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,17 +1,11 @@
 <template>
-  <div>
-    <Editor />
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Editor from "@/components/Editor.vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    Editor
-  }
+  name: "Editor"
 });
 </script>
